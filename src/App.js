afd4f5422cac72b0fe1525a709cfd9d1d791c67b
taku1219 <acmiran189926@gmail.com>
2022-02-28 17:23:35 +0900

    [add]ドラッグ＆ドロップを仮実装

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { Droppable, Draggable, DragDropContext } from 'react-beautiful-dnd';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <DragDropContext>
+        <Droppable droppableId='droppable'>
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              <Draggable draggableId='item0' index={0}>
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    item0
+                  </div>
+                )}
+              </Draggable>
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
 }
 
 export default App;
